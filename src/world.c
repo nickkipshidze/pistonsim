30259d4992f4db3a629ed2ec9766fce5d070c235
NickKipshidze <kipshidze.nick@gmail.com>
2024-12-09 16:47:43 +0400

    Add repeater block logic

--- src/world.c
+++ src/world.c
@@ -57,9 +57,9 @@ void tickWorld(struct Block *world) {
                             getBlockId(worldCpy, x+dirOfs[i][0], y+dirOfs[i][1]) == BLOCK_ID_POWER ||
                             getBlockId(worldCpy, x+dirOfs[i][0], y+dirOfs[i][1]) == BLOCK_ID_WIRE ||
                             (
-                                getBlockId(worldCpy, x+dirOfs[i][0], y+dirOfs[i][1]) == BLOCK_ID_WALL && target -> blockId != BLOCK_ID_WALL
+                                getBlockId(worldCpy, x+dirOfs[i][0], y+dirOfs[i][1]) == BLOCK_ID_WALL && target -> blockId != BLOCK_ID_WALL && target -> blockId != BLOCK_ID_LAMP
                             ) || (
-                                getBlockId(worldCpy, x+dirOfs[i][0], y+dirOfs[i][1]) == BLOCK_ID_LAMP && target -> blockId != BLOCK_ID_LAMP
+                                getBlockId(worldCpy, x+dirOfs[i][0], y+dirOfs[i][1]) == BLOCK_ID_LAMP && target -> blockId != BLOCK_ID_LAMP && target -> blockId != BLOCK_ID_WALL
                             ) || (
                                 getBlockId(worldCpy, x+dirOfs[i][0], y+dirOfs[i][1]) == BLOCK_ID_SWITCH
                             )
@@ -68,9 +68,7 @@ void tickWorld(struct Block *world) {
                         getBlock(worldCpy, x+dirOfs[i][0], y+dirOfs[i][1]) -> data > 1
                     ) {
                         int supply = getBlock(worldCpy, x+dirOfs[i][0], y+dirOfs[i][1]) -> data;
-                        if (supply >= maxSupply) {
-                            maxSupply = supply;
-                        }
+                        if (supply >= maxSupply) maxSupply = supply;
                     }
                     
                     if (
@@ -81,6 +79,25 @@ void tickWorld(struct Block *world) {
                         getBlockId(worldCpy, x+dirOfs[i][0], y+dirOfs[i][1]) == BLOCK_ID_SWITCH
                     ) {
                         connDirs = setBit(connDirs, 3 - i, 1);
+                    }
+
+                    if (getBlockId(worldCpy, x+dirOfs[i][0], y+dirOfs[i][1]) == BLOCK_ID_REPEATER) {
+                        int rotation = getBitRange(getBlock(worldCpy, x+dirOfs[i][0], y+dirOfs[i][1]) -> state, 11, 8);
+                        int supply = getBlock(worldCpy, x+dirOfs[i][0], y+dirOfs[i][1]) -> data;
+
+                        if ((getBit(rotation, 3) || getBit(rotation, 1)) && (i == 0 || i == 2)) {
+                            connDirs = setBit(connDirs, 3 - i, 1);
+                        }
+                        if ((getBit(rotation, 2) || getBit(rotation, 0)) && (i == 1 || i == 3)) {
+                            connDirs = setBit(connDirs, 3 - i, 1);
+                        }
+
+                        if (getBlock(worldCpy, x+dirOfs[i][0], y+dirOfs[i][1]) -> active == true) {
+                            if (getBit(rotation, 3) && i == 2 && supply >= maxSupply) maxSupply = supply;
+                            if (getBit(rotation, 2) && i == 3 && supply >= maxSupply) maxSupply = supply;
+                            if (getBit(rotation, 1) && i == 0 && supply >= maxSupply) maxSupply = supply;
+                            if (getBit(rotation, 0) && i == 1 && supply >= maxSupply) maxSupply = supply;
+                        }
                     }
                 }
 
@@ -97,6 +114,38 @@ void tickWorld(struct Block *world) {
                         target -> data = 0;
                     } else if (target -> data != maxSupply - 1) {
                         target -> data = maxSupply - 1;
+                    }
+                }
+            }
+            else if (target -> blockId == BLOCK_ID_REPEATER) {
+                int rotation = getBitRange(target -> state, 11, 8);
+                int delay = getBitRange(target -> state, 7, 4);
+                int tickCount = getBitRange(target -> state, 3, 0);
+
+                int inputDir;
+                if (getBit(rotation, 3)) inputDir = 2;
+                if (getBit(rotation, 2)) inputDir = 3;
+                if (getBit(rotation, 1)) inputDir = 0;
+                if (getBit(rotation, 0)) inputDir = 1;
+
+                struct Block *input = getBlock(worldCpy, x + dirOfs[inputDir][0], y + dirOfs[inputDir][1]);
+                
+                if ((input -> blockId == BLOCK_ID_POWER || input -> blockId == BLOCK_ID_WIRE || input -> blockId == BLOCK_ID_LAMP || input -> blockId == BLOCK_ID_WALL || input -> blockId == BLOCK_ID_SWITCH || input -> blockId == BLOCK_ID_REPEATER) && input -> active == true && input -> data != 0) {
+                    if (target -> active == false) {
+                        if (tickCount < delay)
+                            target -> state = setBitRange(target -> state, 3, 0, tickCount + 1);
+                        else if (tickCount >= delay) {
+                            target -> active = true;
+                        }
+                    }
+                } else {
+                    if (target -> active == true && tickCount > 0) {
+                        target -> state = setBitRange(target -> state, 3, 0, tickCount - 1);
+                    } else if (target -> active == true && tickCount <= 0) {
+                        target -> active = false;
+                        target -> state = setBitRange(target -> state, 3, 0, 0);
+                    } else if (target -> active == true) {
+                        target -> state = setBitRange(target -> state, 3, 0, delay);
                     }
                 }
             }
