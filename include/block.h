6eca547793a75ffc105d0e9b4272da419f194e67
NickKipshidze <kipshidze.nick@gmail.com>
2024-12-10 22:21:05 +0400

    Add more block constants & helper function definitions

--- include/block.h
+++ include/block.h
@@ -2,8 +2,12 @@
 #define BLOCK_H
 
 extern const int BLOCK_ID_AIR;
+extern const int BLOCK_ID_WALL;
 extern const int BLOCK_ID_POWER;
 extern const int BLOCK_ID_WIRE;
+extern const int BLOCK_ID_LAMP;
+extern const int BLOCK_ID_SWITCH;
+extern const int BLOCK_ID_REPEATER;
 
 struct Block {
     int blockId;
@@ -12,6 +16,9 @@ struct Block {
     bool active;
 };
 
+int getBit(int byte, int index);
+int setBit(int byte, int index, int value);
+int getBitRange(int byte, int startIndex, int endIndex);
 void setBlock(struct Block *world, int x, int y, struct Block block);
 struct Block *getBlock(struct Block *world, int x, int y);
 int getBlockId(struct Block *world, int x, int y);
