e3ad150091df9a593e14a323365f7f69d2cb6e3b
NickKipshidze <kipshidze.nick@gmail.com>
2024-12-10 22:17:07 +0400

    Clean up code & add responsive hotbar

--- src/main.c
+++ src/main.c
@@ -6,33 +6,14 @@
 #include "block.h"
 #include "world.h"
 #include "draw.h"
+#include "control.h"
 
-Vector2 moveVecTo(Vector2 source, Vector2 target, float drift) {
-    float distx = source.x - target.x;
-    float disty = source.y - target.y;
-    return (Vector2){source.x - distx/drift, source.y - disty/drift};
-}
-
-void updateCamera(Camera2D *camera, float *camPosX, float *camPosY) {
-    camera -> offset = (Vector2){WINDOW_WIDTH/2.0, WINDOW_HEIGHT/2.0};
-
-    camera -> target = moveVecTo(
-        camera -> target,
-        (Vector2){*camPosX, *camPosY},
-        8
-    );
-
-    *camPosX += -GetMouseWheelMoveV().x * (2500 + (1000 / camera -> zoom)) * GetFrameTime();
-    *camPosY += -GetMouseWheelMoveV().y * (2500 + (1000 / camera -> zoom)) * GetFrameTime();
-
-    if (IsKeyDown(KEY_LEFT_CONTROL) == true) {
-        if (IsKeyDown(KEY_EQUAL))
-            camera -> zoom += camera -> zoom * 0.9 * GetFrameTime();
-        if (IsKeyDown(KEY_MINUS))
-            camera -> zoom -= camera -> zoom * 0.9 * GetFrameTime();
-        
-        if (camera -> zoom > 4) camera -> zoom = 4;
-    }
+void makeStarterWorld(struct Block *world) {
+    for (int x = WRLDWidth/2-10; x < WRLDWidth/2+11; x++)
+        setBlock(world, x, WRLDWidth/2, (struct Block){BLOCK_ID_WIRE, .state = 0b0101});
+    for (int y = WRLDHeight/2-10; y < WRLDHeight/2+11; y++)
+        setBlock(world, WRLDWidth/2, y, (struct Block){BLOCK_ID_WIRE, .state = 0b1010});
+    setBlock(world, WRLDWidth/2, WRLDHeight/2, (struct Block){.blockId = BLOCK_ID_POWER, .active = true, .data = 9});
 }
 
 int main(int argc, char *argv[]) {
@@ -40,26 +21,17 @@ int main(int argc, char *argv[]) {
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Pistonsim - By Nick");
     SetTargetFPS(120);
 
-    float camPosX = WINDOW_WIDTH/2.0+((WRLDWidth/2.0-7)*WRLDTileSize);
-    float camPosY = WINDOW_HEIGHT/2.0+((WRLDHeight/2.0-3)*WRLDTileSize);
-
     Camera2D camera = {0};
-    camera.target = (Vector2){camPosX, camPosY};
-    camera.rotation = 0.0;
-    camera.zoom = 1.0;
 
     struct Block *world = NULL;
     initWorld(&world);
-
-    for (int x = WRLDWidth/2-10; x < WRLDWidth/2+11; x++)
-        setBlock(world, x, WRLDWidth/2, (struct Block){BLOCK_ID_WIRE, .state = 0b0101});
-    for (int y = WRLDHeight/2-10; y < WRLDHeight/2+11; y++)
-        setBlock(world, WRLDWidth/2, y, (struct Block){BLOCK_ID_WIRE, .state = 0b1010});
-    setBlock(world, WRLDWidth/2, WRLDHeight/2, (struct Block){.blockId = BLOCK_ID_POWER, .active = true, .data = 9});
+    makeStarterWorld(world);
 
     while (!WindowShouldClose()) {
-        updateCamera(&camera, &camPosX, &camPosY);
-
+        updateCamera(&camera);
+        getHotbarInput();
+        getInteractionInput(world, camera);
+        
         WINDOW_WIDTH = GetScreenWidth();
         WINDOW_HEIGHT = GetScreenHeight();
 
@@ -75,7 +47,9 @@ int main(int argc, char *argv[]) {
                 updateWorld(world, camera);
             EndMode2D();
 
-            drawHotbarUI();
+            if (WINDOW_HEIGHT >= 750) drawVHotbarUI();
+            else drawHotbarUI();
+
             if (DEBUG_MODE) drawDebugUI(camera);
         EndDrawing();
     }
